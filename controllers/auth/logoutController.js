b6aa9efeef55a23b7fa763e7092886a2de5e940a
Ashwani Kumar <ashwanikr160@gmail.com>
2022-05-21 23:21:10 +0530

    message status done

--- controllers/auth/logoutController.js
+++ controllers/auth/logoutController.js
@@ -4,6 +4,7 @@ const User = require('../../models/user');
 module.exports = async function (req, res) {
   const { _auth_token } = req.cookies;
   res.clearCookie('_auth_token');
+  console.log(_auth_token);
   res.sendStatus(200);
   try {
     // if (!_auth_token) return res.sendStatus(401);
